c43ca5f2594bfe20b74f037be896044dc94cd808
Alexis Sánchez <aasanchez@gmail.com>
2025-03-21 16:18:34 +0100

    Refactor boot notification request validation

--- validators/BootNotification.go
+++ validators/BootNotification.go
@@ -2,53 +2,59 @@ package validators
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/aasanchez/ocpp16_messages/messages/chargePoint"
 )
 
 func ValidateBootNotificationReq(req chargePoint.BootNotificationReq) error {
-	if err := req.ChargePointModel.Validate(); err != nil {
+	if err := validateRequiredFields(req); err != nil {
 		return err
+	}
+	if err := validateOptionalFields(req); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateRequiredFields(req chargePoint.BootNotificationReq) error {
+	if err := req.ChargePointModel.Validate(); err != nil {
+		return fmt.Errorf("ChargePointModel: %w", err)
 	}
 	if err := req.ChargePointVendor.Validate(); err != nil {
-		return err
+		return fmt.Errorf("ChargePointVendor: %w", err)
 	}
-	if req.ChargeBoxSerialNumber != nil {
-		if err := req.ChargeBoxSerialNumber.Validate(); err != nil {
-			return err
+	return nil
+}
+
+func validateOptionalFields(req chargePoint.BootNotificationReq) error {
+	validators := []struct {
+		name string
+		val  interface {
+			Validate() error
 		}
+	}{
+		{"ChargeBoxSerialNumber", req.ChargeBoxSerialNumber},
+		{"ChargePointSerialNumber", req.ChargePointSerialNumber},
+		{"FirmwareVersion", req.FirmwareVersion},
+		{"Iccid", req.Iccid},
+		{"Imsi", req.Imsi},
+		{"MeterSerialNumber", req.MeterSerialNumber},
+		{"MeterType", req.MeterType},
 	}
-	if req.ChargePointSerialNumber != nil {
-		if err := req.ChargePointSerialNumber.Validate(); err != nil {
-			return err
-		}
-	}
-	if req.FirmwareVersion != nil {
-		if err := req.FirmwareVersion.Validate(); err != nil {
-			return err
-		}
-	}
-	if req.Iccid != nil {
-		if err := req.Iccid.Validate(); err != nil {
-			return err
-		}
-	}
-	if req.Imsi != nil {
-		if err := req.Imsi.Validate(); err != nil {
-			return err
-		}
-	}
-	if req.MeterSerialNumber != nil {
-		if err := req.MeterSerialNumber.Validate(); err != nil {
-			return err
-		}
-	}
-	if req.MeterType != nil {
-		if err := req.MeterType.Validate(); err != nil {
-			return err
+
+	for _, v := range validators {
+		if v.val != nil && !isNil(v.val) {
+			if err := v.val.Validate(); err != nil {
+				return fmt.Errorf("%s: %w", v.name, err)
+			}
 		}
 	}
 	return nil
+}
+
+func isNil(i interface{}) bool {
+	return i == nil || reflect.ValueOf(i).IsNil()
 }
 
 func ValidateBootNotificationConf(conf chargePoint.BootNotificationConf) error {
