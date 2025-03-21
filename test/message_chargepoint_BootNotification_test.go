65e9bacaf305d25e9d89e1919704e3cad78b5510
Alexis Sánchez <aasanchez@gmail.com>
2025-03-21 14:47:50 +0100

    Add test cases for BootNotificationReq validation

--- test/message_chargepoint_BootNotification_test.go
+++ test/message_chargepoint_BootNotification_test.go
@@ -6,8 +6,107 @@ import (
 
 	"github.com/aasanchez/ocpp16_messages/enums"
 	"github.com/aasanchez/ocpp16_messages/messages/chargePoint"
+	"github.com/aasanchez/ocpp16_messages/models"
 	"github.com/aasanchez/ocpp16_messages/validators"
 )
+
+func TestBootNotificationReq(t *testing.T) {
+	tests := []struct {
+		name    string
+		req     chargePoint.BootNotificationReq
+		wantErr bool
+	}{
+		{
+			name: "Valid minimal request",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:  models.CiString20Type("ModelX"),
+				ChargePointVendor: models.CiString20Type("VendorY"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Missing ChargePointModel",
+			req: chargePoint.BootNotificationReq{
+				ChargePointVendor: models.CiString20Type("VendorY"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Missing ChargePointVendor",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel: models.CiString20Type("ModelX"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Empty model",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:  models.CiString20Type(""),
+				ChargePointVendor: models.CiString20Type("VendorY"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Model exceeds 20 characters",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:  models.CiString20Type("123456789012345678901"),
+				ChargePointVendor: models.CiString20Type("VendorY"),
+			},
+			wantErr: true,
+		},
+		{
+			name: "All optional fields valid",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:        "ModelX",
+				ChargePointVendor:       "VendorY",
+				ChargeBoxSerialNumber:   ptr(models.CiString25Type("CB123456789")),
+				ChargePointSerialNumber: ptr(models.CiString25Type("CP123456789")),
+				FirmwareVersion:         ptr(models.CiString50Type("FW1.2.3")),
+				Iccid:                   ptr(models.CiString20Type("ICCID123456")),
+				Imsi:                    ptr(models.CiString20Type("IMSI987654")),
+				MeterSerialNumber:       ptr(models.CiString25Type("MSN0001")),
+				MeterType:               ptr(models.CiString25Type("MTYPE-A")),
+			},
+			wantErr: false,
+		},
+		{
+			name: "FirmwareVersion too long",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:  "ModelX",
+				ChargePointVendor: "VendorY",
+				FirmwareVersion:   ptr(models.CiString50Type("123456789012345678901234567890123456789012345678901")),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Optional field too long: MeterSerialNumber",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:  "ModelX",
+				ChargePointVendor: "VendorY",
+				MeterSerialNumber: ptr(models.CiString25Type("12345678901234567890123456")), // 26 chars
+			},
+			wantErr: true,
+		},
+		{
+			name: "Optional field empty: Iccid",
+			req: chargePoint.BootNotificationReq{
+				ChargePointModel:  "ModelX",
+				ChargePointVendor: "VendorY",
+				Iccid:             ptr(models.CiString20Type("")),
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validators.ValidateBootNotificationReq(tt.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateBootNotificationReq() error = %v, wantErr = %v", err, tt.wantErr)
+			}
+		})
+	}
+}
 
 func TestBootNotificationConf(t *testing.T) {
 	tests := []struct {
