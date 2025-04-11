279dede252ba76aa54760b432acd95c084655908
Alexis Sánchez <aasanchez@gmail.com>
2025-04-11 11:54:47 +0200

    Update function name to ExampleIdTokenType and fix function call

--- types/example_test.go
+++ types/example_test.go
@@ -5,12 +5,14 @@ package types_test
 import (
 	"fmt"
 	"log"
+
+	"github.com/aasanchez/ocpp16messages/types"
 )
 
-func ExampleIdToken() {
+func ExampleIdTokenType() {
 	input := "ABC1234567890XYZ7890" // valid 20-char ASCII input
 
-	idToken, err := IdToken(input)
+	idToken, err := types.IdToken(input)
 	if err != nil {
 		log.Fatalf("Failed to create IdToken: %v", err)
 		return
