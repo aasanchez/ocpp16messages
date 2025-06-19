b09a9558c8e168fb462498694c889dcc264862e8
Alexis Sánchez <aasanchez@gmail.com>
2025-06-19 23:11:54 +0200

    test(benchmark): optimize benchmark loops by using range instead of index for clarity and performance

--- messages/authorize/types/requestPayload_benchmark_test.go
+++ messages/authorize/types/requestPayload_benchmark_test.go
@@ -8,7 +8,7 @@ import (
 
 func BenchmarkRequestPayload_Validate_valid(b *testing.B) {
 	payload := authorizetypes.RequestPayload{IdTag: "B85A-50CBE9678EC6"}
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		if err := payload.Validate(); err != nil {
 			b.Fatal(err)
 		}
@@ -17,7 +17,7 @@ func BenchmarkRequestPayload_Validate_valid(b *testing.B) {
 
 func BenchmarkRequestPayload_Validate_empty(b *testing.B) {
 	payload := authorizetypes.RequestPayload{IdTag: ""}
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		_ = payload.Validate()
 	}
 }
@@ -26,8 +26,9 @@ func BenchmarkRequestPayload_Value(b *testing.B) {
 	payload := authorizetypes.RequestPayload{IdTag: "B85A-50CBE9678EC6"}
 
 	var s string
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		s = payload.Value()
 	}
+
 	_ = s
 }
