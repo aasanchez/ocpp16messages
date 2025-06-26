2b7c9f67c96d755821c17334ee71987bde2271e4
github-action-benchmark <github@users.noreply.github.com>
2025-06-26 16:48:17 +0000

    add Go Benchmark (bootnotification) (go) benchmark result for 05be3e675a08fb78edaadf0de82c21f7fde1ca72

--- bootnotification-benchmarks/data.js
+++ bootnotification-benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750953598438,
+  "lastUpdate": 1750956497055,
   "repoUrl": "https://github.com/aasanchez/ocpp16messages",
   "entries": {
     "Go Benchmark": [
@@ -138,6 +138,50 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "5019027 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "aasanchez",
+            "username": "aasanchez"
+          },
+          "committer": {
+            "name": "aasanchez",
+            "username": "aasanchez"
+          },
+          "id": "05be3e675a08fb78edaadf0de82c21f7fde1ca72",
+          "message": "Bootnotification confirmation",
+          "timestamp": "2025-06-26T15:30:23Z",
+          "url": "https://github.com/aasanchez/ocpp16messages/pull/2/commits/05be3e675a08fb78edaadf0de82c21f7fde1ca72"
+        },
+        "date": 1750956496398,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkRequest_validPayload",
+            "value": 224.5,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "5187124 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequest_validPayload - ns/op",
+            "value": 224.5,
+            "unit": "ns/op",
+            "extra": "5187124 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequest_validPayload - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "5187124 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequest_validPayload - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "5187124 times\n4 procs"
           }
         ]
       }
