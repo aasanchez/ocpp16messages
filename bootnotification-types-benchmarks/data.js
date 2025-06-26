15312d4bdd4ba40d9732877a100bbddfe71e82e9
github-action-benchmark <github@users.noreply.github.com>
2025-06-26 14:13:00 +0000

    add Go Benchmark (go) benchmark result for c0788140c64012d48e15b39087268e56da865a57

---
+++ bootnotification-types-benchmarks/data.js
@@ -0,0 +1,100 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1750947180912,
+  "repoUrl": "https://github.com/aasanchez/ocpp16messages",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Alexis Sánchez",
+            "username": "aasanchez",
+            "email": "aasanchez@gmail.com"
+          },
+          "committer": {
+            "name": "Alexis Sánchez",
+            "username": "aasanchez",
+            "email": "aasanchez@gmail.com"
+          },
+          "id": "c0788140c64012d48e15b39087268e56da865a57",
+          "message": "feat(benchmark): add Go benchmarks for Authorize and Authorize Types with GitHub Actions workflow",
+          "timestamp": "2025-06-26T13:55:20Z",
+          "url": "https://github.com/aasanchez/ocpp16messages/commit/c0788140c64012d48e15b39087268e56da865a57"
+        },
+        "date": 1750947177896,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkRequestPayload_Validate",
+            "value": 4.679,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "256544638 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestPayload_Validate - ns/op",
+            "value": 4.679,
+            "unit": "ns/op",
+            "extra": "256544638 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestPayload_Validate - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "256544638 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestPayload_Validate - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "256544638 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Alexis Sánchez",
+            "username": "aasanchez",
+            "email": "aasanchez@gmail.com"
+          },
+          "committer": {
+            "name": "Alexis Sánchez",
+            "username": "aasanchez",
+            "email": "aasanchez@gmail.com"
+          },
+          "id": "c0788140c64012d48e15b39087268e56da865a57",
+          "message": "feat(benchmark): add Go benchmarks for Authorize and Authorize Types with GitHub Actions workflow",
+          "timestamp": "2025-06-26T13:55:20Z",
+          "url": "https://github.com/aasanchez/ocpp16messages/commit/c0788140c64012d48e15b39087268e56da865a57"
+        },
+        "date": 1750947177896,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkRequestPayload_Validate",
+            "value": 4.679,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "256544638 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestPayload_Validate - ns/op",
+            "value": 4.679,
+            "unit": "ns/op",
+            "extra": "256544638 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestPayload_Validate - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "256544638 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRequestPayload_Validate - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "256544638 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
