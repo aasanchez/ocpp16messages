7c8e68ab43cae3b13040735dc9667f91c94f86bc
Alexis Sánchez <aasanchez@gmail.com>
2025-06-26 22:01:56 +0200

    test(bootnotification): add example tests for Confirmation function to validate output and error handling

--- messages/bootnotification/example_confirmation_test.go
+++ messages/bootnotification/example_confirmation_test.go
@@ -1,1 +1,46 @@
 package bootnotification_test
+
+import (
+	"fmt"
+
+	bn "github.com/aasanchez/ocpp16messages/messages/bootnotification"
+	bootnotificationtypes "github.com/aasanchez/ocpp16messages/messages/bootnotification/types"
+)
+
+func ExampleConfirmation() {
+	input := bootnotificationtypes.ConfirmationPayload{
+		CurrentTime: "2024-08-08T08:08:08Z",
+		Interval:    "42",
+		Status:      "Accepted",
+	}
+
+	conf, err := bn.Confirmation(input)
+	if err != nil {
+		fmt.Println("error:", err)
+
+		return
+	}
+
+	fmt.Println(conf.Interval)
+	fmt.Println(conf.Status.Value())
+	fmt.Println(conf.CurrentTime.String())
+	// Output:
+	// 42
+	// Accepted
+	// 2024-08-08T08:08:08Z
+}
+
+func ExampleConfirmation_invalid() {
+	input := bootnotificationtypes.ConfirmationPayload{
+		CurrentTime: "", // Missing field
+		Interval:    "42",
+		Status:      "Accepted",
+	}
+
+	_, err := bn.Confirmation(input)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	// Output:
+	// error: bootnotificationtypes.Confirmation: invalid payload: confirmation payload: missing required field: CurrentTime
+}
