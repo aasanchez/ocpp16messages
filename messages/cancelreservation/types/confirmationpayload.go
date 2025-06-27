b19049f94c1116c1b6d7d8c1ea6129e781c87e84
Alexis Sánchez <aasanchez@gmail.com>
2025-06-27 16:59:13 +0200

    feat(cancelreservation): implement confirmation message handling with validation and benchmarks for performance testing

--- messages/cancelreservation/types/confirmationpayload.go
+++ messages/cancelreservation/types/confirmationpayload.go
@@ -1,1 +1,19 @@
 package cancelreservationtypes
+
+import (
+	"fmt"
+
+	sharedtypes "github.com/aasanchez/ocpp16messages/shared/types"
+)
+
+type ConfirmationPayload struct {
+	Status string
+}
+
+func (r ConfirmationPayload) Validate() error {
+	if r.Status == "" {
+		return fmt.Errorf(sharedtypes.ErrFmtFieldWrapped, "missing required field: status", sharedtypes.ErrEmptyValueNotAllowed)
+	}
+
+	return nil
+}
