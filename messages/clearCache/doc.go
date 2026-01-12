f2679e23eca0cc913c45ad8c79a7fa412fa1535b
Alexis Sánchez <aasanchez@gmail.com>
2026-01-12 14:20:45 +0100

    Messages Placeholders

---
+++ messages/clearCache/doc.go
@@ -0,0 +1,9 @@
+// ClearCache handling rules:
+//
+// The Central System MAY request a Charge Point to clear its Authorization
+// Cache by sending a ClearCache.req.
+//
+// Upon receipt of ClearCache.req, the Charge Point SHALL respond with
+// ClearCache.conf indicating whether the Authorization Cache was
+// successfully cleared.
+package clearCache
---
+++ messages/getLocalListVersion/doc.go
@@ -0,0 +1,12 @@
+// GetLocalListVersion handling rules:
+//
+// The Central System MAY request the version number of the Charge Point's
+// Local Authorization List by sending GetLocalListVersion.req.
+//
+// Upon receipt, the Charge Point SHALL respond with GetLocalListVersion.conf
+// containing the version number of its Local Authorization List.
+//
+// Version numbers have the following meaning:
+//   - 0   : The local authorization list is empty.
+//   - -1  : The Charge Point does not support Local Authorization Lists.
+package getLocalListVersion
