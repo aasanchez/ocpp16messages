80a124dbea8e7872f37f88e6b59edd91127a1ae0
Alexis Sánchez <aasanchez@gmail.com>
2026-01-26 11:52:30 +0100

    chore: remove old opt-in test filenames
    Clean up deleted benchmark, fuzz, and race test files after renaming to the new naming convention.

--- race/race_all_test.go
+++
@@ -1,9 +0,0 @@
-//go:build race
-
-package race
-
-import "testing"
-
-// Placeholder to enable the race build tag.
-// Run with: make test-race (go test -race -tags=race ./...).
-func TestRacePlaceholder(t *testing.T) {}
